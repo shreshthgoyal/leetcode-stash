a52740e059d0ecb03f81626bbf13acef2b2ef211
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2022-07-04 21:48:52 +0530

    Time: 138 ms (38.42%), Space: 61.2 MB (93.12%) - LeetHub

---
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+       	
+        if(k <= 1) return 0;
+        
+        int prod = 1, res = 0, left = 0;
+        
+        for(int right = 0; right < nums.size(); right++) {
+			
+            prod *= nums[right];
+    
+            while(prod >= k) {
+                prod /= nums[left];
+                left++;
+            }
+			
+			// this res will have number of subarrays(windows) possible
+			/* I/p: [ 10, 5, 2, 6 ], k = 100
+				
+				right = left = 0;
+				prod = prod * 10 = 10; 
+				res = res + 0 - 0 + 1 = 1;  // subarrays - [ 10 ]
+				
+				right = 1;
+				prod = prod * 5 = 10 * 5 = 50
+				res = res + 1 - 0 + 1 = 1 + 1 - 0 + 1 = 3; // subarrays - [ 10 ], [ 5 ], [ 10, 5 ]
+				
+				right = 2;
+				prod = prod * 2 = 50 * 2 = 100;
+				prod > k;
+				prod = prod / nums[left] = prod / nums[0] = 100 / 10 = 10;
+				left++;
+				res = res + 2 - 1 + 1 = 3 + 2 = 5; // subarrays - [ 10 ], [ 5 ], [ 10, 5 ], [ 2 ], [ 5, 2 ] 
+			*/
+            
+            res += right - left + 1;
+        }
+        return res;
+            
+    }
+};
