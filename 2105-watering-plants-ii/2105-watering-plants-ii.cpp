30ffbb43a2a6ff945dcb82e3f76bb0356ff4577c
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2022-07-06 17:20:55 +0530

    Time: 291 ms (13.92%), Space: 78.2 MB (64.98%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.cpp
+++ 2105-watering-plants-ii/2105-watering-plants-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int minimumRefill(vector<int>& plants, int capacityA, int capacityB) {
-        bool check[plants.size()];
         
         int tempA = capacityA, tempB = capacityB;
         
@@ -9,14 +8,14 @@ public:
         
         for(int i=0; i<plants.size()/2; i++)
         {
-            check[i] = tempA < plants[i];
+            ans+= tempA < plants[i];
             
             if(tempA >= plants[i])
                 tempA -= plants[i];
             else
                 tempA = capacityA-plants[i];
 
-            check[plants.size()-1-i] = tempB < plants[plants.size()-1-i];
+            ans+= tempB < plants[plants.size()-1-i];
             
             if(tempB >= plants[plants.size()-1-i])
                 tempB -= plants[plants.size()-1-i];
@@ -26,13 +25,9 @@ public:
         
         if(plants.size()%2)
         {
-            check[plants.size()/2] = max(tempA, tempB) < plants[plants.size()/2];
+            ans+= max(tempA, tempB) < plants[plants.size()/2];
         }
-              
-        
-        for(int i=0; i<plants.size(); i++)
-            ans+=check[i];
-        
+                     
         return ans;
     }
 };
