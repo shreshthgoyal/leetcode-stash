a61b771ee0a5b5a7c5a3411d6a416111842c0082
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2022-07-07 17:42:42 +0530

    Time: 804 ms (58.74%), Space: 104.9 MB (75.59%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -5,20 +5,17 @@ public:
         int ans = 0;
         int temp;
         for(auto &i:piles)
+        {
+            ans+=i;
             pq.push(i);
+        }
         
         while(k--)
         {
            temp = pq.top();
             pq.pop();
-            temp-= temp/2;
-            pq.push(temp);
-        }
-        
-        while(!pq.empty())
-        {
-            ans+=pq.top();
-            pq.pop();
+            ans-= temp/2;
+            pq.push((temp+1)/2);
         }
         
         return ans;
